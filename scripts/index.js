f25078e0e4898dd89ce0dd506e4c9030cbe8e810
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-05-15 02:23:24 -0300

    feat: adding image to form when putting it on the input, and added close popup function

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const characterInput = document.getElementById("image")
-    characterInput.onchange =getImageFromInput;
+    characterInput.onchange = getImageFromInput;
 
     const addCharacter = document.getElementById("add-character")
-    console.log(addCharacter)
     addCharacter.onclick = showPopUp
 
 })
 
-function getImageFromInput(event) { 
+function getImageFromInput(event) {
     const file = event.target.files[0]
+    const popup = document.querySelector(".popup")
+
     const reader = new FileReader()
     reader.readAsDataURL(file);
 
-    reader.onloadend = (event) => {  
-        console.log(event.target.result);
+    reader.onloadend = (loadEndEvent) => {
+        const img = popup.querySelector("#preview-image")
+        img.src = loadEndEvent.target.result
     }
 }
 function showPopUp() {
-    console.log("popup")
     const popup = document.getElementsByClassName("popup")[0]
     popup.classList.add("popup--active")
+}
+
+window.addEventListener("keydown", (event) => {
+    if (event.key != "Escape") return
+    closePopUp()
+})
+
+
+
+function closePopUp() {
+    const popup = document.querySelector(".popup")
+    popup.classList.remove("popup--active")
 }
