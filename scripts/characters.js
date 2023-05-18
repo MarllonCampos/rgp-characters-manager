7f395f1d46f2bf4dcae49793173e93310c1cb394
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-05-18 00:33:10 -0300

    feat: add character scripts and insert id on some inputs

---
+++ scripts/characters.js
@@ -0,0 +1,19 @@
+class Character {
+    create({name,type,image,index})  {
+        const characterContainer = document.createElement("div");
+        const characterImage = document.createElement("img")
+        const characterIncreasePosition = document.createElement("button")
+        const characterDecreasePosition = document.createElement("button")
+    }
+
+    list() {
+        const characterContainer = document.createElement("div");
+        const characterImage = document.createElement("img")
+        const characterIncreasePosition = document.createElement("button")
+        const characterDecreasePosition = document.createElement("button")
+    }
+
+    counter() {
+        
+    }
+}
