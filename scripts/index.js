58162f323f28cad1e4fd2b4bbadf84bc61aca98d
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-05-18 00:35:27 -0300

    feat: add submit form to scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
 
+document.addEventListener("DOMContentLoaded", () => {
     const characterInput = document.getElementById("image")
     characterInput.onchange = getImageFromInput;
 
     const addCharacter = document.getElementById("add-character")
     addCharacter.onclick = showPopUp
 
+    const characterForm = document.getElementById("character-form")
+    characterForm.onsubmit = submitForm
+
 })
+
+function submitForm(event) {
+    event.preventDefault();
+    const image = document.getElementById("preview-image").src
+    if(!image) return alert("Personagem deve ter uma imagem")
+    const elements = event.target.elements
+    const name = elements.name.value
+    const type = Array.from(elements.category).find(category => category.checked).value
+    const character = new Character();
+    character.create({name,type,image,index: 0})
+    
+}
 
 function getImageFromInput(event) {
     const file = event.target.files[0]
@@ -29,7 +44,6 @@ window.addEventListener("keydown", (event) => {
     if (event.key != "Escape") return
     closePopUp()
 })
-
 
 
 function closePopUp() {
