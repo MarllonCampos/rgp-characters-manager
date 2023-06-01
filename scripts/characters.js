0a376488f59b719b4ba2faf6927a6447024a2f6e
Marllon Campos <marllondcsp@gmail.com>
2023-06-01 14:33:41 -0300

    feat: add delete button

--- scripts/characters.js
+++ scripts/characters.js
@@ -3,6 +3,8 @@ class Character {
 
   // MINUS_ICON = `<svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Edit / Remove_Minus"> <path id="Vector" d="M6 12H18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>`;
 
+  DELETE_ICON = `<svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 12V17" stroke="#ff2c06" stroke-width="1.56" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14 12V17" stroke="#ff2c06" stroke-width="1.56" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4 7H20" stroke="#ff2c06" stroke-width="1.56" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 10V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V10" stroke="#ff2c06" stroke-width="1.56" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#ff2c06" stroke-width="1.56" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`;
+
   INCREASE_ICON = `<svg width="22px" height="22px" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg" fill="none" transform="rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 13l-6-6-6 6"></path> </g></svg>`;
 
   DECREASE_ICON = `<svg width="22px" height="22px" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg" fill="none" transform="rotate(0)matrix(1, 0, 0, -1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 13l-6-6-6 6"></path> </g></svg>`;
@@ -75,6 +77,12 @@ class Character {
       characterButtonContainer.appendChild(characterIncreasePosition);
       characterButtonContainer.appendChild(characterDecreasePosition);
 
+      const deleteCharacterButton = document.createElement('button');
+      deleteCharacterButton.setAttribute('type', 'button');
+      deleteCharacterButton.setAttribute('data-character-index', character.index);
+      deleteCharacterButton.classList.add('characters__character-container__delete-character');
+      deleteCharacterButton.innerHTML = this.DELETE_ICON;
+
       const maxAttributes = Object.entries(character.attributes).filter(([key, _value], _) => /^max/gi.test(key));
 
       const attributesContainer = document.createElement('div');
@@ -83,14 +91,14 @@ class Character {
         const maxAttributeButton = document.createElement('button');
         maxAttributeButton.setAttribute('type', 'button');
         maxAttributeButton.setAttribute('value', value);
-        maxAttributeButton.setAttribute('data-aria-attribute', key.toLowerCase());
+        maxAttributeButton.setAttribute('data-attribute', key.toLowerCase());
         maxAttributeButton.textContent = value;
         maxAttributeButton.classList.add('characters__character-container__attribute-button');
 
         const normalAttributeButton = document.createElement('button');
         normalAttributeButton.setAttribute('type', 'button');
         normalAttributeButton.setAttribute('value', value);
-        normalAttributeButton.setAttribute('data-aria-attribute', key.replace(/max/g, '').toLowerCase());
+        normalAttributeButton.setAttribute('data-attribute', key.replace(/max/g, '').toLowerCase());
 
         normalAttributeButton.textContent = value;
         normalAttributeButton.classList.add('characters__character-container__attribute-button');
@@ -102,6 +110,7 @@ class Character {
       characterContainer.appendChild(characterButtonContainer);
       characterContainer.appendChild(characterName);
       characterContainer.appendChild(attributesContainer);
+      characterContainer.appendChild(deleteCharacterButton);
       CHARACTER_SPECIFIC_CONTAINER.appendChild(characterContainer);
     });
   }
